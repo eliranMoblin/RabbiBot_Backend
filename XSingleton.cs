69eadf14d98d533b2dfbf966841a4831367125ab
Eliran Ben lulu <eliran.b@gim.co.il>
2020-03-17 12:32:23 +0200

    Add project files.

---
+++ XSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbiBot_Backend
+{
+    public class XSingleton
+    {
+        public static bool NavBarDisplay = true;
+        private static XSingleton instance = null;
+
+        private XSingleton()
+        {
+        }
+
+        public static XSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new XSingleton();
+                }
+                return instance;
+            }
+        }
+        //public Blazor_Dashboard.Shared.__NavMenu NavMenu;
+
+    }
+}
